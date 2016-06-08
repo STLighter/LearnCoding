3feaedeb8f491a8617559f5afa8d0c826fa98ffd
STLighter <123939775@qq.com>
2016-06-08 20:24:28 +0800

    accept 354D

--- CF354/D. Theseus and labyrinth.js
+++ CF354/D. Theseus and labyrinth.js
@@ -1,48 +1,48 @@
-//(function () {
-	// function Queue (amount) {
-	// 	if(!(this instanceof Queue)) {
-	// 		return new Queue(amount);
-	// 	}
-	// 	amount = amount||64;
-	// 	this.amount = amount;
-	// 	this.bg = 0;
-	// 	this.ed = 0;
-	// 	this.arr = [];
-	// }
-	// Queue.prototype = {
-	// 	constractor: Queue,
-	// 	isEmpty: function () {
-	// 		return (this.bg === this.ed);
-	// 	},
-	// 	doubleSize: function () {
-	// 		var target = [],
-	// 			o = this.arr,
-	// 			i = this.bg,
-	// 			j = this.ed,
-	// 			l = this.amount;
-	// 		target.push(o[i])
-	// 		i=(i+1)%l;
-	// 		for(;i!=j;i=(i+1)%l)
-	// 			target.push(o[i]);
-	// 		this.amount *= 2;
-	// 		this.bg = 0;
-	// 		this.ed = target.length;
-	// 		this.arr = target;
-	// 		//print(this.arr);
-	// 	},
-	// 	push: function (o) {
-	// 		this.arr[this.ed] = o;
-	// 		this.ed = (this.ed+1)%this.amount;
-	// 		// if(this.isEmpty()) {
-	// 		// 	this.doubleSize();
-	// 		// }
-	// 	},
-	// 	pop: function () {
-	// 		var idx = this.bg;
-	// 		this.bg = (this.bg+1)%this.amount;
-	// 		return this.arr[idx];
-	// 	}
-	// }
+(function () {
+	function Queue (amount) {
+		if(!(this instanceof Queue)) {
+			return new Queue(amount);
+		}
+		amount = amount||1024;
+		this.amount = amount;
+		this.bg = 0;
+		this.ed = 0;
+		this.arr = [];
+	}
+	Queue.prototype = {
+		constractor: Queue,
+		isEmpty: function () {
+			return (this.bg === this.ed);
+		},
+		doubleSize: function () {
+			var target = [],
+				o = this.arr,
+				i = this.bg,
+				j = this.ed,
+				l = this.amount;
+			target.push(o[i])
+			i=(i+1)%l;
+			for(;i!=j;i=(i+1)%l)
+				target.push(o[i]);
+			this.amount *= 2;
+			this.bg = 0;
+			this.ed = target.length;
+			this.arr = target;
+			//print(this.arr);
+		},
+		push: function (o) {
+			this.arr[this.ed] = o;
+			this.ed = (this.ed+1)%this.amount;
+			if(this.isEmpty()) {
+				this.doubleSize();
+			}
+		},
+		pop: function () {
+			var idx = this.bg;
+			this.bg = (this.bg+1)%this.amount;
+			return this.arr[idx];
+		}
+	}
 	var rotater = (function () {
 		var _door = [
 			['*'],
@@ -65,24 +65,13 @@
 				};
 			}
 		}
-		function rotateOne (c) {
+		function rotateOne (c, t) {
+			t = (t===undefined?1:t);
 			var di = _todoor[c];
-			return _door[di.x][(di.y+1)%di.l];
-		}
-		function rotate (v) {
-			var ret = [],
-				tmp;
-			for(var i=0,li=v.length;i<li;++i){
-				tmp = "";
-				for(var j=0,lj=v[i].length;j<lj;++j) {
-					tmp+=rotateOne(v[i][j]);
-				}
-				ret.push(tmp);
-			}
-			return ret;
+			return _door[di.x][(di.y+t)%di.l];
 		}
 		return {
-			rotate: rotate
+			rotateOne: rotateOne
 		}
 	})();
 	var steps = [
@@ -137,42 +126,23 @@
 	ed = ed.map(function (e) {
 		return e-1;
 	});
-	var allv = [];
-	allv.push(vec);
-	var i,vtmp;
-	for(i=0;i<3;++i) {
-		vtmp = rotater.rotate(vec);
-		allv.push(vtmp);
-		vec = vtmp;
-	}
-	// allv.forEach(function (e) {
-	// 	print(e);
-	// })
+	
 	st.push(0);
-	//var qu = [];
-	//var qu = new Queue(1000*1000*4+10);
-	var qu = [];
-	var lidx = 0,ridx=1;
-	var amount = 1000*1000*4+10;
+	var qu = new Queue();
 	var used = [];
-	qu[0] = {
+	used.length = n*m*4;
+	qu.push({
 		p: st,
 		l: 0
-	};
+	});
 	used[(st[0]*m+st[1])*4+st[2]] = true;
 
-	
 	var cur;
 	var nxt;
 	var ans = -1;
-	var j,k,x,xx,ii,jj;
-	//var idx = 0;
-	while(/*qu.length > idx*//*!qu.isEmpty()*/lidx!=ridx) {
-		//cur = qu[idx];
-		//cur = qu.pop();
-		cur = qu[lidx];
-		lidx = (lidx+1)%amount;
-		//++idx;
+	var i,j,k,x,xx,ii,jj;
+	while(!qu.isEmpty()) {
+		cur = qu.pop();
 		  // print("i:"+cur.p[0]);
 		  // print("j:"+cur.p[1]);
 		  // print("x:"+cur.p[2]);
@@ -189,33 +159,23 @@
 			ii = i + steps[k][0];
 			jj = j + steps[k][1];
 			if(ii>=0&&ii<n&&jj>=0&&jj<m&&!used[(ii*m+jj)*4+x]) {
-				if(rules[k](allv[x][i][j],allv[x][ii][jj])) {
-					// qu.push({
-					// 	p: [ii,jj,x],
-					// 	l: cur.l+1
-					// });
-					qu[ridx] = {
+				if(rules[k](rotater.rotateOne(vec[i][j],x),rotater.rotateOne(vec[ii][jj],x))) {
+					qu.push({
 						p: [ii,jj,x],
 						l: cur.l+1
-					};
-					ridx = (ridx+1)%amount;
+					});
 					used[(ii*m+jj)*4+x] = true;
 				}
 			}
 		}
 		xx = (x+1)%4;
 		if(!used[(i*m+j)*4+xx]) {
-			// qu.push({
-			// 	p: [i,j,xx],
-			// 	l: cur.l+1
-			// });
-			qu[ridx] = {
+			qu.push({
 				p: [i,j,xx],
 				l: cur.l+1
-			}
-			ridx = (ridx+1)%amount;
+			});
 			used[(i*m+j)*4+xx] = true;
 		}
 	}
 	print(ans);
-//})();
+})();
