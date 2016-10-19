efcd5e5e218e3e79756067d716e9b9699d75e9a1
STLighter <123939775@qq.com>
2016-10-19 12:25:39 +0800

    fin apac 2017 round c

--- Google APAC 2017/round C/D/D.cpp
+++ Google APAC 2017/round C/D/D.cpp
@@ -19,12 +19,48 @@ void useFile(string f) {
 	freopen((f+".in").c_str(),"r",stdin);
 	freopen((f+".out").c_str(),"w",stdout);
 }
-
+int a[4010],d[4010],n;
+bool used[4010];
+vector<int> m[4010];
+bool dfs() {
+    if(n == 0) {
+        return 0;
+    }
+    int x = 0,y = 0;
+    for(int i=0;i<n;++i) {
+        x = max(x, a[i]);
+        y = max(y, d[i]);
+    }
+    int l = 0;
+    for(int i=0;i<n;++i) {
+        if(a[i]!=x&&d[i]!=y) {
+            a[l] = a[i];
+            d[l] = d[i];
+            ++l;
+            continue;
+        }
+        if(a[i]==x&&d[i]==y)
+            return 1;
+    }
+    n = l;
+    return dfs();
+}
 void gao(){
+    scanf("%d",&n);
+    for(int i=0;i<n;++i) {
+        used[i] = 0;
+        scanf("%d%d",&a[i],&d[i]);
+    }
+    if(dfs()) {
+        printf("YES\n");
+        return;
+    }
+    printf("NO\n");
+    return;
 }
 int main()
 {
-	//useFile("D-large-practice");
+	useFile("D-large-practice");
 	int t;
     scanf("%d",&t);
     for(int i=1;i<=t;++i) {
